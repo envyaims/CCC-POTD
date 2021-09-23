8ca29c02c212407ce90ae31f3ac9b08f07fad4f8
envyaims <79723780+envyaims@users.noreply.github.com>
2021-09-22 20:41:51 -0400

    Create 01.cpp

---
+++ 2021 September/01.cpp
@@ -0,0 +1,73 @@
+// Author: envifly
+// Problem: B. Falling Anvils from Codeforces - Codeforces Beta Round #69 (Div. 1 Only)
+// URL: https://codeforces.com/problemset/problem/77/B
+// Limits: 2000 ms, 256 MB
+// Attempted: 2021-09-01 10:13:43 EST
+ 
+#ifndef LOCAL
+#define debug(...) 0
+#include <bits/stdc++.h>
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#include "C:\programmingfunnyxd\includeordie.h"
+#endif
+using namespace std;
+inline namespace readio { const int BSZ = 1<<15; char ibuf[BSZ]; int ipos, ilen;char nc() {if (ipos == ilen) {ipos = 0; ilen = fread(ibuf,1,BSZ,stdin);if (!ilen) return EOF;}return ibuf[ipos++];}
+template<class T> void ri(T& x) {char ch; int sgn = 1;while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');x *= sgn;}
+template<class T> void rc(T& x){ x = nc();while(isspace(x))x = nc();}template<class T, class... Ts> void ri(T& t, Ts&... ts) {ri(t);ri(ts...);}
+template<class str>void rs(str& x) { char ch; while (isspace(ch = nc()));do { x += ch; } while (!isspace(ch = nc()) && ch != EOF);}}
+template<typename T> void max_self(T& a, T b){a = max(a,b);}template<typename T> void min_self(T& a, T b){a = min(a,b);}
+struct minsecond{bool operator()(pair<int,int> a, pair<int,int> b){return a.second > b.second;}};
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;template<typename T> using maxpq = priority_queue<T>;
+typedef priority_queue<pair<int,int>, vector<pair<int,int>>, minsecond> secondpq;
+typedef long long ll; const ll INF = 1e18; const int MOD = 1e9+7;
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define print(x) cout<<x<<"\n"
+#define mp(x,y) make_pair(x,y)
+#define np(x) next_permutation(all(x))
+#define F first
+#define S second
+#define sz(v) int(v.size())
+#define bpop(x) __builtin_popcount(x)
+#define fillarr(arr,n,am) fill(arr[0],arr[n],am)
+#define readarr(arr, n)for(int i = 0; i < n; i++)ri(arr[i])
+#define readchararr(arr, n)for(int i = 0; i < n; i++)rc(arr[i])
+#define readgrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)ri(arr[i][j]);
+#define readchargrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)rc(arr[i][j]);
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,-1,1};
+#define int long long
+#define double long double
+ 
+void doStuff(){
+	int a,b; ri(a,b);
+	if(b == 0){
+		print(1);
+		return;
+	}
+	if(a == 0){
+		print(0.5);
+		return;
+	}
+	int area = a * (2*b);
+	double upper = 1.0 * a / 4;
+	double left = b * a;
+	double right;
+	if(4*b >= a){
+		right = (upper * a)/2; // intersects with x = a
+	}
+	else{
+		right = a * b - (4*b*b)/2; // intersects with y = b
+	}
+	cout<<setprecision(69)<<(left+right)/area<<endl;
+	
+}
+ 
+signed main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);int t = 1;
+	ri(t);
+	while(t--)doStuff();
+    #ifdef LOCAL
+    cerr << "Runtime: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+}
