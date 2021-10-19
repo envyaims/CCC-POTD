9da6b9d3dd0b8d47d3f8a89fc5b58fc19a28092c
envifly <79723780+envyaims@users.noreply.github.com>
2021-10-19 07:50:11 -0400

    Create 18-hard.cpp

---
+++ 2021 October/18-hard.cpp
@@ -0,0 +1,79 @@
+// Author: envy
+// Problem: C. Random Events from Codeforces - Codeforces Round #689 (Div. 2, based on Zed Code Competition)
+// URL: https://codeforces.com/problemset/problem/1461/C
+// Limits: 2000 ms, 256 MB
+// Attempted: 2021-10-18 19:01:30 EST
+
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#endif
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T> void max_self(T& a, T b){a = max(a,b);}
+template<typename T> void min_self(T& a, T b){a = min(a,b);}
+struct minsecond{bool operator()(pair<int,int> a, pair<int,int> b){return a.second > b.second;}};
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using maxpq = priority_queue<T>;
+using secondpq = priority_queue<pair<int,int>, vector<pair<int,int>>, minsecond>;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll; const ll INF = 2e18; const int MOD = 1e9+7;
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define print(x) cout<<x<<"\n"
+#define mp(x,y) make_pair(x,y)
+#define np(x) next_permutation(all(x))
+#define F first
+#define S second
+#define sz(v) int(v.size())
+#define tobin(b,x) bitset<b>(x).to_string()
+const int dx[8] = {1,-1,0,0,-1,-1,1,1}, dy[8] = {0,0,-1,1,1,-1,-1,1};
+#define int long long
+int pow(int a,int b,int m){int ans=1;while(b){if(b&1){ans=(ans*a)%m;}b>>=1;a=(a*a)%m;}return ans;}
+
+void doStuff(){
+	int n,m; cin>>n>>m;
+	vector<int> nums(n);
+	for(int& i: nums)cin>>i;
+	vector<pair<int,double>> prob(m);
+	for(int i = 0; i < m; i++){
+		int a; double b; cin>>a>>b;
+		prob[i] = mp(a,b);
+	}
+	int sortuntil = 0;
+	for(int i = 0; i < n; i++){
+		if(nums[i] != i+1){
+			sortuntil = i+1;
+		}
+	}
+	if(sortuntil == 0){
+		print(1);
+		return;
+	}
+	debug(prob);
+	double ans = 1;
+	for(int i = 0; i < m; i++){
+		if(prob[i].F >= sortuntil){
+			ans *= 1-prob[i].S;
+			debug(prob[i],ans);
+		}
+	}
+	cout<<fixed<<1-ans<<"\n";
+	
+	
+}
+
+signed main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	cin>>t;
+	while(t--){
+		doStuff();
+	}
+}
