8d9b98fa3e9c48b5b197ad7f4d19309bf930b7ed
envifly <79723780+envyaims@users.noreply.github.com>
2021-10-19 07:49:44 -0400

    Create 18-easy.cpp

---
+++ 2021 October/18-easy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin>>n;
+    if(n%5==0){
+        cout<<n/5;
+    }
+    else{
+        cout<<n/5+1;
+    }
+    return 0;
+
+}
