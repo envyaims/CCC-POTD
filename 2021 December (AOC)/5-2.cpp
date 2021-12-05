3453c366df457e3c84e58b5b7740b7503c5c7fd5
envifly <79723780+envyaims@users.noreply.github.com>
2021-12-05 00:33:14 -0500

    Create 5-2.cpp

---
+++ 2021 December (AOC)/5-2.cpp
@@ -0,0 +1,79 @@
+#ifndef LOCAL
+#include <bits/stdc++.h>
+#define debug(...) 0
+#else
+#include "/Users/envyaims/Documents/template/stdc++.h"
+#include "/Users/envyaims/Documents/template/debug.cpp"
+#endif
+using namespace std;
+#define int long long
+int cnt[1000][1000];
+template<typename T>
+vector<string> split(T str, char sep){
+	vector<string> output;
+	size_t start = 0, end = 0;
+	while((end = str.find(sep,start)) != string::npos){
+		if(end != start)output.push_back(str.substr(start,end-start));
+		start = end+1;
+	}
+	
+	if(end != start)output.push_back(str.substr(start));
+	return output;
+	
+}
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	while(1){
+		if(cin.eof())break;
+		string a,b,c; cin>>a>>b>>c;
+		vector<string> pa = split(a,','),  pb = split(c,',');
+		if(pa[0] == pb[0] && pa[1] != pb[1]){
+			int apos = stoi(pa[1]), bpos = stoi(pb[1]);
+			for(int j = min(apos,bpos);j <= max(apos,bpos);j++){
+				cnt[stoi(pa[0])][j]++;
+			}
+		}
+		else if(pa[1] == pb[1] && pa[0] != pb[0]){
+			int apos = stoi(pa[0]), bpos = stoi(pb[0]);
+			for(int j = min(apos,bpos); j <= max(apos,bpos); j++){
+				cnt[j][stoi(pa[1])]++;
+			}
+		}
+		else{
+			if(stoi(pa[1]) - stoi(pb[1]) == stoi(pa[0] )- stoi(pb[0])){
+				int apos = stoi(pa[0]), bpos = stoi(pb[0]);
+				if(stoi(pa[1]) - stoi(pb[1]) < 0){
+					swap(pa,pb);
+					swap(apos,bpos);
+				}
+				for(int i = 0; i <=abs(apos-bpos); i++){
+					cnt[stoi(pa[0])-i][stoi(pa[1])-i]++;
+				}
+			}
+			else if(abs(stoi(pa[1]) - stoi(pb[1]) )== abs(stoi(pa[0] )- stoi(pb[0]))){
+				int apos = stoi(pa[0]), bpos = stoi(pb[0]);
+				int dist = abs(apos-bpos);
+				if(apos < bpos){
+					for(int i = 0; i <= dist; i++){
+						cnt[apos+i][stoi(pa[1])-i]++;
+					}
+				}
+				else{
+					for(int i = 0; i <= dist; i++){
+						cnt[apos-i][stoi(pa[1])+i]++;
+					}
+				}
+			}
+		}
+	}
+	int ans = 0;
+	for(int i = 0; i< 1000; i++){
+		for(int j = 0; j < 1000; j++){
+			if(cnt[i][j]>1)ans++;
+		}
+	}
+	cout<<ans;
+	
+}
