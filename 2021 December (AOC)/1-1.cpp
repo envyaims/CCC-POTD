0d7530128d15abe7b7041c4500c78a195d01e1ef
envifly <79723780+envyaims@users.noreply.github.com>
2021-12-04 21:25:22 -0500

    Create 1-1.cpp

---
+++ 2021 December (AOC)/1-1.cpp
@@ -0,0 +1,25 @@
+#ifndef LOCAL
+#include <bits/stdc++.h>
+#define debug(...) 0
+#else
+#include "/Users/envyaims/Documents/template/stdc++.h"
+#include "/Users/envyaims/Documents/template/debug.cpp"
+#endif
+using namespace std;
+#define int long long
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	vector<int> nums;
+	while(1){
+		if(cin.eof())break;
+		int a; cin>>a;
+		nums.push_back(a);
+	}
+	int ans = 0;
+	for(int i = 0; i < nums.size()-1; i++){
+		if(nums[i+1] > nums[i])ans++;
+	}
+	cout<<ans;
+	
+}
