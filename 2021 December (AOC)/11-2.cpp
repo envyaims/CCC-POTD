0475ac9c6206d0768b6d052fb40827b79e2285f4
envifly <79723780+envyaims@users.noreply.github.com>
2021-12-11 00:35:19 -0500

    Create 11-2.cpp

---
+++ 2021 December (AOC)/11-2.cpp
@@ -0,0 +1,77 @@
+#ifndef LOCAL
+#include <bits/stdc++.h>
+#define debug(...) 0
+#else
+#include "/Users/envyaims/Documents/template/stdc++.h"
+#include "/Users/envyaims/Documents/template/debug.cpp"
+#endif
+using namespace std;
+#define int long long
+char grid[10][10];
+int ngrid[10][10];
+bool flashed[10][10];
+const int dx[8] = {1,-1,0,0,-1,-1,1,1}, dy[8] = {0,0,-1,1,1,-1,-1,1};
+
+int ans = 0;
+int n = 10;
+
+void simflash(int i,int j){
+	for(int k = 0; k < 8; k++){
+			int r = dx[k]+i;
+			int c = dy[k]+j;
+			if(r < 0 || r >= n || c < 0 || c >= n)continue;
+			ngrid[r][c]++;
+			if(ngrid[r][c] > 9 && !flashed[r][c]){
+				ans++;
+				flashed[r][c] = true;
+				simflash(r,c);
+			}
+		}
+}
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cin>>grid[i][j];
+		}
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			ngrid[i][j] = grid[i][j] - '0';
+		}
+	}
+	for(int step =1;; step++){
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				ngrid[i][j]++;
+				flashed[i][j] = false;
+				
+			}
+		}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(ngrid[i][j] > 9 && !flashed[i][j]){
+					ans++;
+					flashed[i][j] = true;
+					simflash(i,j);
+				}
+			}
+		}
+		int cnt = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(flashed[i][j]){
+					cnt++;
+					ngrid[i][j] = 0;
+				}
+			}
+		}
+		if(cnt == 100){
+			cout<<step;
+			break;
+		}
+	}
+	
+	
+}
